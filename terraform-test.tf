6f7effe736ee8b364758cc408d2dcc72073cf75d
ramab <ramab@us.ibm.com>
2016-11-15 11:48:08 -0500

    more updates

--- terraform-test.tf
+++ terraform-test.tf
@@ -15,8 +15,9 @@ resource "softlayer_virtual_guest" "terraform-test-01" {
     name = "test-01"
     domain = "example.com"
     ssh_keys = ["${softlayer_ssh_key.test-key.id}"]
-    os_reference_code = "UBUNTU_14_64"
+    image = "UBUNTU_14_64"
     region = "mex01"
+    local_disk = true
     public_network_speed = 10
     hourly_billing = true
     cpu = 4
