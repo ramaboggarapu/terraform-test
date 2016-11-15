65333212e91dcf0178caea7f6123ca61d2d05cef
ramab <ramab@us.ibm.com>
2016-11-15 11:28:48 -0500

    update variables

--- terraform-test.tf
+++ terraform-test.tf
@@ -1,17 +1,20 @@
+variable "username" {}
+variable "api_key" {}
+
 provider "softlayer" {
     username = "${var.username}"
     api_key = "${var.api_key}"
 }
 
-resource "softlayer_ssh_key" "kube-key" {
-    label = "kube-key"
+resource "softlayer_ssh_key" "test-key" {
+    label = "test-key"
     public_key = "${file("terraform-test.pub")}"
 }
 
-resource "softlayer_virtual_guest" "kube-master-01" {
-    hostname = "kube-01"
+resource "softlayer_virtual_guest" "terraform-test-01" {
+    hostname = "test-01"
     domain = "example.com"
-    ssh_key_ids = ["${softlayer_ssh_key.kube-key.id}"]
+    ssh_key_ids = ["${softlayer_ssh_key.test-key.id}"]
     os_reference_code = "UBUNTU_14_64"
     datacenter = "mex01"
     network_speed = 1000
@@ -19,7 +22,7 @@ resource "softlayer_virtual_guest" "kube-master-01" {
     cores = 4
     memory = 1024
     provisioner "local-exec" {
-        command = "echo ${softlayer_virtual_guest.kube-master-01.ipv4_address} >> hosts"
+        command = "echo ${softlayer_virtual_guest.terraform-test-01.ipv4_address} >> hosts"
     }
 
 }
