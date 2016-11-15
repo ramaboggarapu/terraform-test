661465c9b9088a66366fa6c31a72737f1a529cef
ramab <ramab@us.ibm.com>
2016-11-15 11:44:16 -0500

    update variables

--- terraform-test.tf
+++ terraform-test.tf
@@ -7,20 +7,20 @@ provider "softlayer" {
 }
 
 resource "softlayer_ssh_key" "test-key" {
-    label = "test-key"
+    name = "test-key"
     public_key = "${file("terraform-test.pub")}"
 }
 
 resource "softlayer_virtual_guest" "terraform-test-01" {
-    hostname = "test-01"
+    name = "test-01"
     domain = "example.com"
-    ssh_key_ids = ["${softlayer_ssh_key.test-key.id}"]
+    ssh_keys = ["${softlayer_ssh_key.test-key.id}"]
     os_reference_code = "UBUNTU_14_64"
-    datacenter = "mex01"
-    network_speed = 1000
+    region = "mex01"
+    public_network_speed = 10
     hourly_billing = true
-    cores = 4
-    memory = 1024
+    cpu = 4
+    ram = 1024
     provisioner "local-exec" {
         command = "echo ${softlayer_virtual_guest.terraform-test-01.ipv4_address} >> hosts"
     }
